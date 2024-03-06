32bbb65261c95384190342b4352e7931f64a42d6
Grace Gutierrez <119556733+faithgrace7@users.noreply.github.com>
2024-03-06 11:11:15 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+<style>
+
+    h1 {
+        font-family: Ariel;
+        text-align: center;
+    }
+  
+   body {
+        font-size: 20pt;
+        text-align: center;
+        font-family: Calibri;
+        background-image: url('https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1&fbclid=IwAR2hMqLvog4seVhkfHkhU1YEifJI3_P2HfNnSjcJxCtRtrNJwfb3TyHyjrI');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: 100% 100%;
+    }
+
+    img{
+        width: 50%;
+    }
+    
+  </style>
+  
+  <title>Static Website</title>
+
+  <head>
+  </head>
+  <body>
+    
+    <h1>Faith Grace Gutierrez</h1>
+    <p> 
+      Hii! my full name is Faith Grace Ortiz Gutierrez, (Pit) for short.<br>20 years of age, and turning 21 this coming 6th of July. <br>Currently living in barangay Carmen, Macanhan, Zone 9, Phase 4.<br>
+      <br>A sophomore BS-IT at USTP. <br>I really love organizing things. <br>Also a big fan of vintage collections and loves capturing  moments. <br>That's all. Thank you!</br></br></br></br>
+    </p>
+    <img src="https://scontent.fcgy3-1.fna.fbcdn.net/v/t1.15752-9/423455042_761342962322953_4493000921438100853_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeEMwphhlsX3BHeKrYDlEA2rmZguf8uJP1KZmC5_y4k_UqgxtLH4W7hxyMliwJ3t8pwxJps7TGl4McZiVTOi9mhr&_nc_ohc=7VOGl57Y9HkAX9qJuYf&_nc_ht=scontent.fcgy3-1.fna&oh=03_AdQqAmHxaagGuPyfCK-VYmMP2g1bl1OhcfP5a35C3g3ZkA&oe=660E9EF5"><br><br>     
+ 
+
+</style>  
+</body>
+</html>
